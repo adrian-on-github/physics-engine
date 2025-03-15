941790dc7eeccf2b2c2ca2a7d0b0aa5da1bd298a
Adrian <adrian.hassan.ef@gmail.com>
2025-03-16 00:52:38 +0100

    Adding Ball velocity, friction and acceleration in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext("2d");
 const Balls = [];
 
 let LEFT, UP, RIGHT, DOWN;
+let friction = 0.1;
 
 class Ball {
   constructor(x, y, r) {
@@ -14,7 +15,9 @@ class Ball {
     this.r = r;
     this.vel_x = 0;
     this.vel_y = 0;
-    this.velocity = 4;
+    this.acc_x = 0;
+    this.acc_y = 0;
+    this.acceleration = 0.1;
     this.player = false;
     Balls.push(this);
   }
@@ -27,9 +30,23 @@ class Ball {
     ctx.fillStyle = "red";
     ctx.fill();
   }
+
+  display() {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.acc_x * 1000, this.y + this.acc_y * 1000);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.vel_x * 100, this.y + this.vel_y * 100);
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+  }
 }
 
 function keyControl(b) {
+  // SETS UP, DOWN, LEFT, RIGHT TRUE IF PRESS KEYDOWN
   canvas.addEventListener("keydown", (event) => {
     if (event.keyCode === 87) {
       // W
@@ -49,6 +66,7 @@ function keyControl(b) {
     }
   });
 
+  // SETS UP, DOWN, LEFT, RIGHT FALSE IF PRESS KEYUP
   canvas.addEventListener("keyup", (event) => {
     if (event.keyCode === 87) {
       // W
@@ -68,24 +86,29 @@ function keyControl(b) {
     }
   });
 
-  if (UP) {
-    b.vel_y = -b.velocity;
-  }
-  if (DOWN) {
-    b.vel_y = b.velocity;
-  }
   if (LEFT) {
-    b.vel_x = -b.velocity;
+    b.acc_x = -b.acceleration;
+  }
+  if (UP) {
+    b.acc_y = -b.acceleration;
   }
   if (RIGHT) {
-    b.vel_x = b.velocity;
+    b.acc_x = b.acceleration;
+  }
+  if (DOWN) {
+    b.acc_y = b.acceleration;
   }
   if (!UP && !DOWN) {
-    b.vel_y = 0;
+    b.acc_y = 0;
   }
   if (!RIGHT && !LEFT) {
-    b.vel_x = 0;
+    b.acc_x = 0;
   }
+
+  b.vel_x += b.acc_x;
+  b.vel_y += b.acc_y;
+  b.vel_x *= 1 - friction;
+  b.vel_y *= 1 - friction;
   b.x += b.vel_x;
   b.y += b.vel_y;
 }
@@ -98,12 +121,12 @@ function mainLoop() {
     if (b.player) {
       keyControl(b);
     }
+    b.display();
   });
   requestAnimationFrame(mainLoop);
 }
 
 let Ball1 = new Ball(100, 100, 30);
-let Ball2 = new Ball(200, 90, 20);
 Ball1.player = true;
 
 requestAnimationFrame(mainLoop);
