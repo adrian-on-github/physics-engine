d731ef8f99c9b7aa9b65c79b5d9279cafedfc490
Adrian <adrian.hassan.ef@gmail.com>
2025-03-16 23:10:07 +0100

    Adding Vector class working to index.js

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ class Vector {
   subtract(v) {
     return new Vector(this.x - v.x, this.y - v.y);
   }
+
+  // Satz des Pythagoras
+  mag() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  mult(n) {
+    return new Vector(this.x * n, this.y * n);
+  }
+
+  drawVec(start_x, start_y, n, color) {
+    ctx.beginPath();
+    ctx.moveTo(start_x, start_y);
+    ctx.lineTo(start_x + this.x * n, start_y + this.y * n);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+  }
 }
 
 class Ball {
@@ -27,11 +44,9 @@ class Ball {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.vel_x = 0;
-    this.vel_y = 0;
-    this.acc_x = 0;
-    this.acc_y = 0;
-    this.acceleration = 0.1;
+    this.vel = new Vector(0, 0);
+    this.acc = new Vector(0, 0);
+    this.acceleration = 0.5;
     this.player = false;
     Balls.push(this);
   }
@@ -46,16 +61,8 @@ class Ball {
   }
 
   display() {
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x + this.acc_x * 1000, this.y + this.acc_y * 1000);
-    ctx.strokeStyle = "green";
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x + this.vel_x * 100, this.y + this.vel_y * 100);
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
+    this.vel.drawVec(this.x, this.y, 50, "green");
+    this.acc.drawVec(this.x, this.y, 200, "blue");
   }
 }
 
@@ -101,30 +108,28 @@ function keyControl(b) {
   });
 
   if (LEFT) {
-    b.acc_x = -b.acceleration;
+    b.acc.x = -b.acceleration;
   }
   if (UP) {
-    b.acc_y = -b.acceleration;
+    b.acc.y = -b.acceleration;
   }
   if (RIGHT) {
-    b.acc_x = b.acceleration;
+    b.acc.x = b.acceleration;
   }
   if (DOWN) {
-    b.acc_y = b.acceleration;
+    b.acc.y = b.acceleration;
   }
   if (!UP && !DOWN) {
-    b.acc_y = 0;
+    b.acc.y = 0;
   }
   if (!RIGHT && !LEFT) {
-    b.acc_x = 0;
+    b.acc.x = 0;
   }
 
-  b.vel_x += b.acc_x;
-  b.vel_y += b.acc_y;
-  b.vel_x *= 1 - friction;
-  b.vel_y *= 1 - friction;
-  b.x += b.vel_x;
-  b.y += b.vel_y;
+  b.vel = b.vel.add(b.acc);
+  b.vel = b.vel.mult(1 - friction);
+  b.x += b.vel.x;
+  b.y += b.vel.y;
 }
 
 function mainLoop() {
