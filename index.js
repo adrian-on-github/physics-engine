9bd673d566317d8345ab603beb09700ec12ad071
Adrian <adrian.hassan.ef@gmail.com>
2025-03-20 17:42:06 +0100

    one before final

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-"use strict";
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const Balls = [];
-const Walls = [];
+const BALLZ = [];
+const WALLZ = [];
 
 let LEFT, UP, RIGHT, DOWN;
-let friction = 0.1;
+let friction = 0.05;
 
 class Vector {
   constructor(x, y) {
@@ -15,7 +13,6 @@ class Vector {
     this.y = y;
   }
 
-  // v = Vector, n = Number
   add(v) {
     return new Vector(this.x + v.x, this.y + v.y);
   }
@@ -33,20 +30,15 @@ class Vector {
   }
 
   normal() {
-    return new Vector(this.y, -this.x).unit();
+    return new Vector(-this.y, this.x).unit();
   }
 
-  //returns a vector with same direction and 1 length
   unit() {
     if (this.mag() === 0) {
       return new Vector(0, 0);
     } else {
       return new Vector(this.x / this.mag(), this.y / this.mag());
     }
-  }
-
-  static dot(v1, v2) {
-    return v1.x * v2.x + v1.y * v2.y;
   }
 
   drawVec(start_x, start_y, n, color) {
@@ -57,27 +49,9 @@ class Vector {
     ctx.stroke();
     ctx.closePath();
   }
-}
 
-class Matrix {
-  constructor(rows, cols) {
-    this.rows = rows;
-    this.cols = cols;
-    this.data = [];
-
-    for (let i = 0; i < this.rows; i++) {
-      this.data[i] = [];
-      for (let j = 0; j < this.cols; j++) {
-        this.data[i][j] = 0;
-      }
-    }
-  }
-
-  multiplyVec(vec) {
-    let result = new Vector(0, 0);
-    result.x = this.data[0][0] * vec.x + this.data[0][1] * vec.y;
-    result.y = this.data[1][0] * vec.x + this.data[1][1] * vec.y;
-    return result;
+  static dot(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y;
   }
 }
 
@@ -91,12 +65,12 @@ class Ball {
     } else {
       this.inv_m = 1 / this.m;
     }
+    this.elasticity = 1;
     this.vel = new Vector(0, 0);
     this.acc = new Vector(0, 0);
-    this.acceleration = 0.35;
+    this.acceleration = 0.5;
     this.player = false;
-    this.elasticity = 2;
-    Balls.push(this);
+    BALLZ.push(this);
   }
 
   drawBall() {
@@ -105,15 +79,15 @@ class Ball {
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.fillStyle = "red";
-    // ctx.fill();
+    ctx.fill();
     ctx.closePath();
   }
 
   display() {
-    this.vel.drawVec(this.pos.x, this.pos.y, 20, "green");
+    this.vel.drawVec(this.pos.x, this.pos.y, 10, "green");
     ctx.fillStyle = "black";
-    ctx.fillText("m = " + this.m, this.pos.x - 10, this.pos.y + 5);
-    ctx.fillText("m = " + this.elasticity, this.pos.x - 10, this.pos.y + 5);
+    ctx.fillText("m = " + this.m, this.pos.x - 10, this.pos.y - 5);
+    ctx.fillText("e = " + this.elasticity, this.pos.x - 10, this.pos.y + 5);
   }
 
   reposition() {
@@ -122,138 +96,99 @@ class Ball {
     this.vel = this.vel.mult(1 - friction);
     this.pos = this.pos.add(this.vel);
   }
-
-  keyControl() {
-    if (LEFT) {
-      this.acc.x = -this.acceleration;
-    }
-    if (UP) {
-      this.acc.y = -this.acceleration;
-    }
-    if (RIGHT) {
-      this.acc.x = this.acceleration;
-    }
-    if (DOWN) {
-      this.acc.y = this.acceleration;
-    }
-    if (!UP && !DOWN) {
-      this.acc.y = 0;
-    }
-    if (!RIGHT && !LEFT) {
-      this.acc.x = 0;
-    }
-  }
 }
 
+//Walls are line segments between two points
 class Wall {
-  constructor(x1, x2, y1, y2) {
+  constructor(x1, y1, x2, y2) {
     this.start = new Vector(x1, y1);
     this.end = new Vector(x2, y2);
-    this.center = this.start.add(this.end).mult(0.5);
-    this.length = this.end.subtr(this.start).mag();
-    this.refStart = new Vector(x1, y1);
-    this.refEnd = new Vector(x2, y2);
-    this.refUnit = this.end.subtr(this.start).unit();
-    this.angle = 0;
-    this.angVel = 0;
-    Walls.push(this);
-    console.log(this);
+    WALLZ.push(this);
   }
 
   drawWall() {
-    let rotMat = rotMx(this.angle);
-    let newDir = rotMat.multiplyVec(this.refUnit);
-    this.start = this.center.add(newDir.mult(-this.length / 2));
-    this.end = this.center.add(newDir.mult(this.length / 2));
     ctx.beginPath();
     ctx.moveTo(this.start.x, this.start.y);
     ctx.lineTo(this.end.x, this.end.y);
     ctx.strokeStyle = "black";
     ctx.stroke();
+    ctx.closePath();
   }
 
   wallUnit() {
     return this.end.subtr(this.start).unit();
   }
+}
 
-  keyControl() {
-    if (LEFT) {
-      this.angVel = -0.5;
+function keyControl(b) {
+  canvas.addEventListener("keydown", function (e) {
+    if (e.keyCode === 65) {
+      LEFT = true;
     }
+    if (e.keyCode === 87) {
+      UP = true;
+    }
+    if (e.keyCode === 68) {
+      RIGHT = true;
+    }
+    if (e.keyCode === 83) {
+      DOWN = true;
+    }
+  });
 
-    if (RIGHT) {
-      this.angVel = 0.5;
+  canvas.addEventListener("keyup", function (e) {
+    if (e.keyCode === 65) {
+      LEFT = false;
     }
+    if (e.keyCode === 87) {
+      UP = false;
+    }
+    if (e.keyCode === 68) {
+      RIGHT = false;
+    }
+    if (e.keyCode === 83) {
+      DOWN = false;
+    }
+  });
+
+  if (LEFT) {
+    b.acc.x = -b.acceleration;
   }
-
-  reposition() {
-    this.angle += this.angVel;
-    this.angVel *= 0.3;
+  if (UP) {
+    b.acc.y = -b.acceleration;
+  }
+  if (RIGHT) {
+    b.acc.x = b.acceleration;
+  }
+  if (DOWN) {
+    b.acc.y = b.acceleration;
+  }
+  if (!LEFT && !RIGHT) {
+    b.acc.x = 0;
+  }
+  if (!UP && !DOWN) {
+    b.acc.y = 0;
   }
 }
 
-function userInput(b) {
-  // SETS UP, DOWN, LEFT, RIGHT TRUE IF PRESS KEYDOWN
-  canvas.addEventListener("keydown", (event) => {
-    if (event.keyCode === 87) {
-      // W
-      UP = true;
-    }
-    if (event.keyCode === 65) {
-      // A
-      LEFT = true;
-    }
-    if (event.keyCode === 83) {
-      // S
-      DOWN = true;
-    }
-    if (event.keyCode === 68) {
-      // D
-      RIGHT = true;
-    }
-  });
-
-  // SETS UP, DOWN, LEFT, RIGHT FALSE IF PRESS KEYUP
-  canvas.addEventListener("keyup", (event) => {
-    if (event.keyCode === 87) {
-      // W
-      UP = false;
-    }
-    if (event.keyCode === 65) {
-      // A
-      LEFT = false;
-    }
-    if (event.keyCode === 83) {
-      // S
-      DOWN = false;
-    }
-    if (event.keyCode === 68) {
-      // D
-      RIGHT = false;
-    }
-  });
+function round(number, precision) {
+  let factor = 10 ** precision;
+  return Math.round(number * factor) / factor;
 }
 
 function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function rotMx(angle) {
-  let mx = new Matrix(2, 2);
-  mx.data[0][0] = Math.cos(angle);
-  mx.data[0][1] = -Math.sin(angle);
-  mx.data[1][0] = Math.sin(angle);
-  mx.data[1][1] = Math.cos(angle);
-  return mx;
-}
-
+//returns with the closest point on a line segment to a given point
 function closestPointBW(b1, w1) {
   let ballToWallStart = w1.start.subtr(b1.pos);
-  let wallEndToBall = b1.pos.subtr(w1.end);
-
   if (Vector.dot(w1.wallUnit(), ballToWallStart) > 0) {
     return w1.start;
-  } else if (Vector.dot(w1.wallUnit(), wallEndToBall) > 0) {
+  }
+
+  let wallEndToBall = b1.pos.subtr(w1.end);
+  if (Vector.dot(w1.wallUnit(), wallEndToBall) > 0) {
     return w1.end;
   }
 
@@ -270,6 +205,7 @@ function coll_det_bb(b1, b2) {
   }
 }
 
+//collision detection between ball and wall
 function coll_det_bw(b1, w1) {
   let ballToClosest = closestPointBW(b1, w1).subtr(b1.pos);
   if (ballToClosest.mag() <= b1.r) {
@@ -285,6 +221,7 @@ function pen_res_bb(b1, b2) {
   b2.pos = b2.pos.add(pen_res.mult(-b2.inv_m));
 }
 
+//penetration resolution between ball and wall
 function pen_res_bw(b1, w1) {
   let penVect = b1.pos.subtr(closestPointBW(b1, w1));
   b1.pos = b1.pos.add(penVect.unit().mult(b1.r - penVect.mag()));
@@ -304,6 +241,7 @@ function coll_res_bb(b1, b2) {
   b2.vel = b2.vel.add(impulseVec.mult(-b2.inv_m));
 }
 
+//collision response between ball and wall
 function coll_res_bw(b1, w1) {
   let normal = b1.pos.subtr(closestPointBW(b1, w1)).unit();
   let sepVel = Vector.dot(b1.vel, normal);
@@ -312,56 +250,86 @@ function coll_res_bw(b1, w1) {
   b1.vel = b1.vel.add(normal.mult(-vsep_diff));
 }
 
-// function momentum_display() {
-// let momentum = Ball1.vel.add(Ball2.vel).mag();
-// ctx.fillText(`Momentum:` + `${momentum.toFixed(3)}`, 1400, 530);
-// }
+function momentum_display() {
+  let momentum = Ball1.vel.add(Ball2.vel).mag();
+  ctx.fillText("Momentum: " + round(momentum, 4), 500, 330);
+}
 
-function mainLoop() {
+function mainLoop(timestamp) {
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-  userInput();
-  Balls.forEach((b, index) => {
+  BALLZ.forEach((b, index) => {
     b.drawBall();
     if (b.player) {
-      b.keyControl;
+      keyControl(b);
     }
-    Walls.forEach((w) => {
-      if (coll_det_bw(Balls[index], w)) {
-        pen_res_bw(Balls[index], w);
-        coll_res_bw(Balls[index], w);
+    //each ball object iterates through each wall object
+    WALLZ.forEach((w) => {
+      if (coll_det_bw(BALLZ[index], w)) {
+        pen_res_bw(BALLZ[index], w);
+        coll_res_bw(BALLZ[index], w);
       }
     });
-    for (let i = index + 1; i < Balls.length; i++) {
-      if (coll_det_bb(Balls[index], Balls[i])) {
-        pen_res_bb(Balls[index], Balls[i]);
-        coll_res_bb(Balls[index], Balls[i]);
+    for (let i = index + 1; i < BALLZ.length; i++) {
+      if (coll_det_bb(BALLZ[index], BALLZ[i])) {
+        pen_res_bb(BALLZ[index], BALLZ[i]);
+        coll_res_bb(BALLZ[index], BALLZ[i]);
       }
     }
-
     b.display();
     b.reposition();
   });
-  // momentum_display();
 
-  Walls.forEach((w) => {
+  //drawing each wall on the canvas
+  WALLZ.forEach((w) => {
     w.drawWall();
-    w.keyControl();
-    w.reposition();
   });
 
   requestAnimationFrame(mainLoop);
 }
 
-// for (let i = 0; i < 10; i++) {
-//   let newBall = new Ball(
-//     randInt(100, 1300),
-//     randInt(50, 600),
-//     randInt(20, 50),
-//     randInt(0, 10)
-//   );
-//   newBall.elasticity = randInt(0, 10) / 10;
-// }
+for (let i = 0; i < 10; i++) {
+  let newBall = new Ball(
+    randInt(100, 500),
+    randInt(50, 400),
+    randInt(20, 50),
+    randInt(0, 10)
+  );
+  newBall.elasticity = randInt(0, 10) / 10;
+}
 
-let Wall1 = new Wall(1200, 500, 600, 300);
+//walls along the canvas edges
+let edge1 = new Wall(0, 0, canvas.clientWidth, 0);
+let edge2 = new Wall(
+  canvas.clientWidth,
+  0,
+  canvas.clientWidth,
+  canvas.clientHeight
+);
+let edge3 = new Wall(
+  canvas.clientWidth,
+  canvas.clientHeight,
+  0,
+  canvas.clientHeight
+);
+let edge4 = new Wall(0, canvas.clientHeight, 0, 0);
+for (let i = 0; i < 10; i++) {
+  let newBall = new Ball(
+    randInt(300, 1200),
+    randInt(50, 700),
+    randInt(20, 80),
+    randInt(0, 50)
+  );
+  newBall.elasticity = randInt(0, 50) / 10;
+}
+for (let i = 0; i < 7; i++) {
+  let newWall = new Wall(
+    randInt(50, 1200),
+    randInt(50, 600),
+    randInt(10, 2000),
+    randInt(50, 300)
+  );
+  newWall.elasticity = randInt(0, 10) / 10;
+}
+BALLZ[0].player = true;
 
 requestAnimationFrame(mainLoop);
